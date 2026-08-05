49d2862dbc6bfa3260a7763e8d85a1ee0f28f854
Dhananjayan S <148212474+DJ-Dhananjayan@users.noreply.github.com>
2026-08-05 10:19:41 +0530

    Time: 0 ms (100%), Space: 19.3 MB (72.4%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,14 +1,18 @@
 class Solution:
-    def solve(self, ans: list[str], s: str, open: int, close: int, n: int) -> None:
-        if close == n and open == n:
-            ans.append(s)
-            return
-        if open < n:
-            self.solve(ans, s + "(", open + 1, close, n)
-        if close < open:
-            self.solve(ans, s + ")", open, close + 1, n)
-    
-    def generateParenthesis(self, n: int) -> list[str]:
-        ans = []                    
-        self.solve(ans, "", 0, 0, n)  
-        return ans
+    def generateParenthesis(self, n: int) -> List[str]:
+        res=[]
+        t=[]
+        def back(l,r):
+            if l==r==n:
+                res.append("".join(t))
+                return
+            if l<n:
+                t.append("(")
+                back(l+1,r)
+                t.pop()
+            if r<l:
+                t.append(")")
+                back(l,r+1)
+                t.pop()
+        back(0,0)
+        return res
