1cf06cad23785426a3a6426930d8ea516d8d5c99
Dhananjayan S <148212474+DJ-Dhananjayan@users.noreply.github.com>
2026-08-12 09:05:54 +0530

    Time: 0 ms (100%), Space: 19.2 MB (59.63%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        bit = 1
+        curr = num
+
+        while curr > 0:
+            num ^= bit
+            
+            bit <<= 1
+            curr >>= 1
+
+        return num
+        
